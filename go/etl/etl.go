139514ed9b0afb579e83c89e8712d55634ad5099
Andrew Sardone <andrew@andrewsardone.com>
2014-04-19 15:40:33 -0400

    Implement passing etl in Go lang

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,13 @@
 package etl
 
-func Transform(l map[int][]string) map[string]int {
-	return nil
+import "strings"
+
+func Transform(oldScoring map[int][]string) map[string]int {
+	m := make(map[string]int)
+	for score, letters := range oldScoring {
+		for _, l := range letters {
+			m[strings.ToLower(l)] = score
+		}
+	}
+	return m
 }
