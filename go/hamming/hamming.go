955b874cf56d47c04b6baa7f8f1de6b693b4ec49
Andrew Sardone <andrew@andrewsardone.com>
2014-04-19 17:13:10 -0400

    Implement passing hamming distance Go lang function
    This was a first attempt at getting all of the tests to pass.

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,13 @@
 package hamming
 
 func Distance(strandA, strandB string) int {
-	return 0
+	distance := 0
+	for i, r := range strandA {
+		if i < len(strandA) && i < len(strandB) {
+			if string(r) != string(strandB[i]) {
+				distance++
+			}
+		}
+	}
+	return distance
 }
