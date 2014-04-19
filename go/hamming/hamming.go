acf93de921e8282bd7cd5e1ee47c56db260a2cfc
Andrew Sardone <andrew@andrewsardone.com>
2014-04-19 17:30:18 -0400

    [go] Name hamming distance result
    A little Go idiom I picked up, but you can add a name to the result
    within a function declaration, removing the need to initialize a
    variable within the function body.
    
    http://golang.org/ref/spec#Function_types

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,6 @@
 package hamming
 
-func Distance(strandA, strandB string) int {
-	distance := 0
+func Distance(strandA, strandB string) (distance int) {
 	for i := 0; i < len(strandA) && i < len(strandB); i++ {
 		if strandA[i] != strandB[i] {
 			distance++
