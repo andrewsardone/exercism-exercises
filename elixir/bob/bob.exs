31bce31fa25d1b140503b8427e2e38e967b30a63
Andrew Sardone <andrew@andrewsardone.com>
2014-04-20 13:03:50 -0400

    [elixir] Remove literal "1, 2, 3" check from Teenager
    Submission: http://exercism.io/submissions/713784c9d034488eacab66cd

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,9 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      input == "1, 2, 3" -> "Whatever."
       String.strip(input) == "" -> "Fine. Be that way!"
       String.ends_with?(input, "?") -> "Sure."
-      String.upcase(input) == input -> "Woah, chill out!"
+      String.upcase(input) == input && Regex.run(%r/\p{L}/, input) -> "Woah, chill out!"
       true -> "Whatever."
     end
   end
