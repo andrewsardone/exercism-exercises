5e86b24a21872273f34c16006f8c1b7833286a68
Andrew Sardone <andrew@andrewsardone.com>
2014-04-20 13:10:35 -0400

    [elixir] Extract Teenager conds to private functions
    Submission: http://exercism.io/submissions/aa9ad0a052870316e15268e4

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,18 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      String.strip(input) == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.upcase(input) == input && Regex.run(%r/\p{L}/, input) -> "Woah, chill out!"
-      true -> "Whatever."
+      is_empty?(input)      -> "Fine. Be that way!"
+      is_a_question?(input) -> "Sure."
+      is_yelling?(input)    -> "Woah, chill out!"
+      true                  -> "Whatever."
     end
+  end
+
+  defp is_empty?(s), do: String.strip(s) == ""
+
+  defp is_a_question?(s), do: String.ends_with?(s, "?")
+
+  defp is_yelling?(s) do
+    String.upcase(s) == s && Regex.run(%r/\p{L}/, s)
   end
 end
