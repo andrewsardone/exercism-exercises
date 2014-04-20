c276aa88a1d60c0846fd2e65997edb92f31a4282
Andrew Sardone <andrew@andrewsardone.com>
2014-04-20 12:31:19 -0400

    [elixir] Merge bob conditions into general-ish cases
    This still contains a literal condition, so we need to work around that.
    
    Submission: http://exercism.io/submissions/7fadf6f1774fb4df69b2580d

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,15 +1,10 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      input == "WATCH OUT!" -> "Woah, chill out!"
-      input == "Does this cryogenic chamber make me look fat?" -> "Sure."
-      input == "1, 2, 3 GO!" -> "Woah, chill out!"
-      input == "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!" ->  "Woah, chill out!"
-      input == "I HATE YOU" ->  "Woah, chill out!"
-      input == "УХОДИТЬ" ->  "Woah, chill out!"
-      input == "" -> "Fine. Be that way!"
-      input == "  " -> "Fine. Be that way!"
-      input == "4?" -> "Sure."
+      input == "1, 2, 3" -> "Whatever."
+      String.strip(input) == "" -> "Fine. Be that way!"
+      String.ends_with?(input, "?") -> "Sure."
+      String.upcase(input) == input -> "Woah, chill out!"
       true -> "Whatever."
     end
   end
