2fdf78b67b90624ef35b84bbc9b8def7eb3b3a44
Andrew Sardone <andrew@andrewsardone.com>
2014-04-20 11:56:50 -0400

    [elixir] Pass bob tests w/ trivial implementation

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,16 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      input == "WATCH OUT!" -> "Woah, chill out!"
+      input == "Does this cryogenic chamber make me look fat?" -> "Sure."
+      input == "1, 2, 3 GO!" -> "Woah, chill out!"
+      input == "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!" ->  "Woah, chill out!"
+      input == "I HATE YOU" ->  "Woah, chill out!"
+      input == "УХОДИТЬ" ->  "Woah, chill out!"
+      input == "" -> "Fine. Be that way!"
+      input == "  " -> "Fine. Be that way!"
+      input == "4?" -> "Sure."
+      true -> "Whatever."
     end
   end
 end
