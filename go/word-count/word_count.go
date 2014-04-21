94fdc593a73d57c657159457dba9eeba230c9541
Andrew Sardone <andrew@andrewsardone.com>
2014-04-21 07:58:00 -0400

    [go] Implement WordCount function w/ passing tests
    I like how Go's Map type starts with default values that are relevant to
    the type:
    
    ```go
    m := make(map[string]int)
    m["foo"] == 0
    
    s := make(map[string]string)
    s["bar"] == ""
    ```
    
    I do *not* care for Go's lack of functional primitives like `map`.
    Writing a for loop to append to some slice feels rough. Perhaps I'm
    missing something.
    
    Submission: http://exercism.io/submissions/9b23000be2c7b44f8eefbfad

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,5 +1,29 @@
 package wc
 
+import (
+	"regexp"
+	"strings"
+)
+
 func WordCount(sentence string) Histogram {
-	return nil
+	words := collectWords(sentence)
+	return countWords(words)
+}
+
+func collectWords(sentence string) []string {
+	r, _ := regexp.Compile(`\w+`)
+	words := r.FindAllString(sentence, -1)
+	lowercaseWords := []string{}
+	for _, w := range words {
+		lowercaseWords = append(lowercaseWords, strings.ToLower(w))
+	}
+	return lowercaseWords
+}
+
+func countWords(words []string) map[string]int {
+	wordCount := make(map[string]int)
+	for _, word := range words {
+		wordCount[word] = wordCount[word] + 1
+	}
+	return wordCount
 }
