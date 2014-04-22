708cbb7040169c1cb2bac9a8c3d8d897f2dbe6aa
Andrew Sardone <andrew@andrewsardone.com>
2014-04-22 08:09:30 -0400

    [elixir] Impelement ListOps.reduce w/ passing tests
    No need for private helper functions; we just need to split our public
    function between two scenarios.

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -38,9 +38,8 @@ defmodule ListOps do
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-
-  end
+  def reduce([], acc, _), do: acc
+  def reduce([h|t], acc, f), do: reduce(t, f.(h, acc), f)
 
   @spec append(list, list) :: list
   def append(a, b) do
