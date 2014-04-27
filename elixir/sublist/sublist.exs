4675aee52f596733f5e5c82ccc59960ba5bd614a
Andrew Sardone <andrew@andrewsardone.com>
2014-04-26 20:07:47 -0400

    [elixir] Implement real sublist check
    The readability is a little less than ideal with the two functions and
    their various pattern matching clauses, but I think it's a relatively
    elegant solution.

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -5,12 +5,35 @@ defmodule Sublist do
   """
   def compare(a, b) do
     case { length(a), length(b) } do
-      { la, lb } when la < lb ->
-        :sublist
-      { la, lb } when la > lb ->
-        :superlist
+      { len_a, len_b } when len_a < len_b ->
+        if is_sublist?(a, b, len_a, len_b), do: :sublist, else: :unequal
+      { len_a, len_b } when len_a > len_b ->
+        if is_sublist?(b, a, len_b, len_a), do: :superlist, else: :unequal
       _ ->
         if a == b, do: :equal, else: :unequal
+    end
+  end
+
+  # Returns true if the first list is a sublist of the second second list,
+  # using pattern matching testing for:
+  #   1. The first list is empty condition
+  #   2. Heads of the two lists are equal, so we'll recursively test the tails
+  #   3. The final catch-all condition that will return false because we didn't
+  #      have, at least, equal hads
+  defp is_sublist?([], _), do: true
+  defp is_sublist?([h|ta], [h|tb]), do: is_sublist?(ta, tb)
+  defp is_sublist?(_, _), do: false
+
+  # The controller function delegates to is_sublist?/2 for recursive head/tail
+  # checking, but by decrementing the len_b parameter and checking if the
+  # result is less than len_a, it sets up a guard to return false.
+  defp is_sublist?(_, _, len_a, len_b) when len_a > len_b, do: false
+  defp is_sublist?(a, b, len_a, len_b) do
+    if is_sublist?(a, b) do
+      true
+    else
+      [_|tail_b] = b
+      is_sublist?(a, tail_b, len_a, len_b-1)
     end
   end
 end
