643ab953864d49f7820b61ed354223dc60211af9
Andrew Sardone <andrew@andrewsardone.com>
2014-05-02 07:53:05 -0400

    [elixir] Refactor RNA conversion to be less trivial
    Submission: http://exercism.io/submissions/eb63541509b95c83ff06a9c0

--- elixir/rna-transcription/dna.exs
+++ elixir/rna-transcription/dna.exs
@@ -9,13 +9,16 @@ defmodule DNA do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    case dna do
-      'G' -> 'C'
-      'C' -> 'G'
-      'T' -> 'A'
-      'A' -> 'U'
-      'ACGTGGTCTTAA' -> 'UGCACCAGAAUU'
-      _ -> 'UGAC'
-    end
+    do_dna_rna_conversion(dna)
+  end
+
+  defp do_dna_rna_conversion([]), do: []
+  defp do_dna_rna_conversion([h|t]) do
+    [case h do
+       ?G -> ?C
+       ?C -> ?G
+       ?T -> ?A
+       ?A -> ?U
+     end|do_dna_rna_conversion(t)]
   end
 end
