b27a15c66da25295d8bb6faf8bf7c9d85372221a
Andrew Sardone <andrew@andrewsardone.com>
2014-05-03 21:59:34 -0400

    [elixir] Tweak bogus tabs in generated source

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -4,20 +4,20 @@ defmodule DNA do
 
   ## Examples
 
-	iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
-	4
-	"""
-	@spec hamming_distance([char], [char]) :: non_neg_integer
-	def hamming_distance(strand1, strand2) do
-      calc_hamming_distance(strand1, strand2, 0)
-	end
+  iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
+  4
+  """
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) do
+    calc_hamming_distance(strand1, strand2, 0)
+  end
 
-    defp calc_hamming_distance([], _, accm), do: accm
-    defp calc_hamming_distance(_, [], accm), do: accm
-    defp calc_hamming_distance([h|t1], [h|t2], accm) do
-      calc_hamming_distance t1, t2, accm
-    end
-    defp calc_hamming_distance([_|t1], [_|t2], accm) do
-      calc_hamming_distance t1, t2, accm+1
-    end
+  defp calc_hamming_distance([], _, accm), do: accm
+  defp calc_hamming_distance(_, [], accm), do: accm
+  defp calc_hamming_distance([h|t1], [h|t2], accm) do
+    calc_hamming_distance t1, t2, accm
+  end
+  defp calc_hamming_distance([_|t1], [_|t2], accm) do
+    calc_hamming_distance t1, t2, accm+1
+  end
 end
