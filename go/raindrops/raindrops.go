d496b627fa8c52bac6dc27a3d060c1199c157d18
Andrew Sardone <andrew@andrewsardone.com>
2014-05-06 07:16:35 -0400

    [go] Refactor raindrops to use string concatenation
    Per @aprescott's [suggestion][1].
    
    [1] :http://exercism.io//submissions/13787de337163c97c1fb7441

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,25 +2,24 @@ package raindrops
 
 import (
 	"strconv"
-	"strings"
 )
 
 func Convert(input int) string {
-	rainDrops := []string{}
+	rainDrops := ""
 
 	if input%3 == 0 {
-		rainDrops = append(rainDrops, "Pling")
+		rainDrops += "Pling"
 	}
 	if input%5 == 0 {
-		rainDrops = append(rainDrops, "Plang")
+		rainDrops += "Plang"
 	}
 	if input%7 == 0 {
-		rainDrops = append(rainDrops, "Plong")
+		rainDrops += "Plong"
 	}
 
 	if len(rainDrops) == 0 {
-		rainDrops = append(rainDrops, strconv.Itoa(input))
+		rainDrops = strconv.Itoa(input)
 	}
 
-	return strings.Join(rainDrops, "")
+	return rainDrops
 }
