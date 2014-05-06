3d726a59024bf40caecba7c7390144c21761379f
Andrew Sardone <andrew@andrewsardone.com>
2014-05-05 22:11:17 -0400

    [go] Implement passing tests for raindrop convert
    Submission: http://exercism.io/submissions/13787de337163c97c1fb7441

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,26 @@
 package raindrops
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Convert(input int) string {
-	return ""
+	rainDrops := []string{}
+
+	if input%3 == 0 {
+		rainDrops = append(rainDrops, "Pling")
+	}
+	if input%5 == 0 {
+		rainDrops = append(rainDrops, "Plang")
+	}
+	if input%7 == 0 {
+		rainDrops = append(rainDrops, "Plong")
+	}
+
+	if len(rainDrops) == 0 {
+		rainDrops = append(rainDrops, strconv.Itoa(input))
+	}
+
+	return strings.Join(rainDrops, "")
 }
