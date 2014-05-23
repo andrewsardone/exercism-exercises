a59203c807ec0d3abea008ece50cde6e3844dd87
Andrew Sardone <andrew@andrewsardone.com>
2014-05-23 16:20:17 -0400

    Remove unnecessary named return value
    As [pointed out][1] by @mmm444
    
    [1]: http://exercism.io/submissions/c3d4e9e5f3bf4522266c768a
    
    Submission: http://exercism.io/submissions/7f17c0fb26e2caa1712a7559

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -6,7 +6,7 @@ func Square(input int) uint64 {
 	return 1 << uint(input-1)
 }
 
-func Total() (total uint64) {
+func Total() uint64 {
 	// Since we're totalling all the grains on the chessboard (64 squares),
 	// this equals a 64 bit unsigned integer with all 1 bits, i.e., the
 	// MaxUint64.
